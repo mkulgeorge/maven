1e43fd7f5d351a7b5f042342e7de4abc36a61a33
mkulgeorge <kulandai.george@gmail.com>
2020-04-14 13:20:57 +0530

    commit files

---
+++ src/test/java/com/analyzemydata/MavenReleasePluginTest/AppTest.java
@@ -0,0 +1,11 @@
+package com.analyzemydata.MavenReleasePluginTest;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	public void getHelloWorld() {
+		System.out.println("Printing Hello World!");
+    }
+}
